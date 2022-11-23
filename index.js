35bdb7bcf019a40ae9d222fe2de9b359f2a77382
smahaduzzaman <smahaduzzaman96@gmail.com>
2022-11-23 22:09:35 +0600

    Third Commit For Update Project Initial Setup and Running Well

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-alert('Product Resale Website');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello Product Reseller Site!');
+});
+
+app.listen(port, () => {
+    console.log(`Product Reseller App Running at http://localhost:${port}`);
+});
