783d7598839c8146a1bd3873c90a7b44037f4c0f
smahaduzzaman <smahaduzzaman96@gmail.com>
2022-11-25 02:45:40 +0600

    Fifth Commit For Update Data File

--- index.js
+++ index.js
@@ -6,10 +6,42 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const cars = require('./data/cars.json');
+const categories = require('./data/categories.json');
+
 app.get('/', (req, res) => {
-    res.send('Hello Product Reseller Site!');
+    res.send('Hello Xclusive Cars!');
 });
 
+app.get('/cars', async (req, res) => {
+    res.send(cars);
+})
+
+app.get('/cars/:id', async (req, res) => {
+    const car = cars.find(car => car.id === req.params.id);
+    res.send(car);
+})
+
+app.get('/categories', async (req, res) => {
+    res.send(categories);
+})
+
+app.get('/category/:id', (req, res) => {
+    const { id } = req.params;
+    const car = cars.filter((car) => car.category.catId === id);
+    res.send(car);
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '100106') {
+        res.send(cars);
+    } else {
+        const category = cars.filter(ct => ct.catId === id);
+        res.send(category);
+    }
+})
+
 app.listen(port, () => {
-    console.log(`Product Reseller App Running at http://localhost:${port}`);
+    console.log(`Xclusive Cars App Running at http://localhost:${port}`);
 });
